e76a8f672769287bc4faa83e66b792d924817131
Iván Vargas O <ivanvargas.va@gmail.com>
2025-10-29 12:21:57 -0500

    refactor: improve JS code

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 const btnMenu = document.getElementById('btnMenu');
 const btnCloseMenu = document.getElementById('btnCloseMenu');
 const nav = document.getElementById('nav');
-
-
-/* creación backdrop */
 const backdrop = document.createElement("div");
-backdrop.classList.add("backdrop");
-document.body.appendChild(backdrop);
 
 
-/* Funciones */
-btnMenu.addEventListener('click' , ()=>{
+function openMenu() {
     nav.classList.add('active');
     backdrop.classList.add("active");
-});
+}
 
-btnCloseMenu.addEventListener('click' , ()=>{
+function closeMenu() {
     nav.classList.remove('active');
     backdrop.classList.remove("active");
-});
+}
+
+function createBackdrop() {
+    backdrop.classList.add("backdrop");
+    document.body.appendChild(backdrop);
+}
 
 
+createBackdrop();
+btnMenu.addEventListener('click' , openMenu);
+btnCloseMenu.addEventListener('click' , closeMenu);
